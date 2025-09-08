db9173122837125cacb21f741d73f2553b9ee315
JJeong Kim <64342840+jeongwoo0427@users.noreply.github.com>
2025-09-08 20:55:22 +0900

    Update sample_text.dart

---
+++ lib/widgets/sample_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SampleText extends StatelessWidget {
+  const SampleText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Sample Text');
+  }
+}
