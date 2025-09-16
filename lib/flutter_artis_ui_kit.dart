dd2a220eb11335fca0c1e0093dc8ae88aeb450e1
JJeong Kim <64342840+jeongwoo0427@users.noreply.github.com>
2025-09-16 20:03:00 +0900

    feat: AFloatingNavigationBar

--- lib/flutter_artis_ui_kit.dart
+++ lib/flutter_artis_ui_kit.dart
@@ -4,3 +4,4 @@ import 'dart:async';
 part 'widgets/a_card/a_card.dart';
 part 'widgets/text/a_sample_text.dart';
 part 'widgets/a_card/a_button/a_button.dart';
+part 'widgets/a_floating_bottom_navigation_bar.dart';
---
+++ lib/widgets/a_floating_bottom_navigation_bar.dart
@@ -0,0 +1,172 @@
+part of '../flutter_artis_ui_kit.dart';
+
+class AFloatingNavigationBar extends StatefulWidget {
+  ///It is items what you want to add
+  List<AFloatingNavigationItem> items;
+
+  ///
+  int selectedIndex;
+
+  ///You can take what is current index when you tap navigation item
+  Function(int index) onTap;
+
+  ///A double that determines how far it is from the bottom
+  double bottomMargin;
+
+  ///Determines how much evocation effect you want to add.
+  double elevation;
+
+  ///Decide the padding you want to put inside of NavigationBar
+  EdgeInsets padding;
+
+  ///Decide how much flexion you want to give to the corner.
+  BorderRadius? borderRadius;
+
+  ///Set up how fast movement of Cursor
+  Duration duration;
+
+  ///The Curve of item movement
+  Curve? curves;
+
+  ///You can change the navigationbar color, but default color follows at the 'ThemeData.ColorScheme.SurfaceColor'
+  Color? backgroundColor;
+
+  double itemWidth;
+
+  double itemHeight;
+
+  AFloatingNavigationBar(
+      {
+        Key? key,
+        required this.items,
+        required this.selectedIndex,
+        required this.onTap,
+        this.bottomMargin = 15,
+        this.elevation = 5,
+        this.padding = const EdgeInsets.only(right: 14, left: 14, top: 10, bottom: 2),
+        this.duration = const Duration(milliseconds: 300),
+        this.curves,
+        this.borderRadius,
+        this.backgroundColor,
+        this.itemWidth = 40,
+        this.itemHeight = 40,
+      })
+      : super(key: key);
+
+  @override
+  State<AFloatingNavigationBar> createState() => _MainMenuBottomFloatingBarState();
+}
+
+class _MainMenuBottomFloatingBarState extends State<AFloatingNavigationBar> with TickerProviderStateMixin {
+
+  final double _cursorWidth = 5;
+
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+
+    List<Widget> navigationItems = [];
+    for (int i = 0; i < widget.items.length; i++) {
+      navigationItems.add(_getNavigationItem(i, widget.items[i], widget.onTap));
+    }
+
+    return Container(
+      margin: EdgeInsets.only(bottom: widget.bottomMargin),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: widget.padding,
+            decoration: BoxDecoration(
+              borderRadius: widget.borderRadius ?? BorderRadius.circular(30),
+              color: widget.backgroundColor ?? themeData.colorScheme.surface,
+              boxShadow: [BoxShadow(color: themeData.colorScheme.shadow.withOpacity(0.17), blurRadius: 35,spreadRadius: 1, offset: const Offset(0, 7))]
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 5,
+                  width: widget.itemWidth*navigationItems.length,
+                  child: Stack(
+                    children: [
+                      AnimatedPositioned(
+                          left: _getCurrentCursorPosition(widget.itemWidth, _cursorWidth, navigationItems.length, widget.selectedIndex),
+                          top: 0,
+                          bottom: 0,
+                          duration: Duration(milliseconds: 200),
+                          curve: widget.curves??Curves.easeInOut,
+                          child: Container(
+                            width: _cursorWidth,
+                            decoration: BoxDecoration(color: themeData.colorScheme.primary, borderRadius: BorderRadius.circular(10)),
+                          )),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: navigationItems,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getNavigationItem(int index, AFloatingNavigationItem item, Function(int index) onTap) {
+    ThemeData themeData = Theme.of(context);
+    Color? itemColor = widget.selectedIndex == index ? themeData.colorScheme.primary : null;
+    return Container(
+      width: widget.itemWidth,
+      height: widget.itemHeight,
+      child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            customBorder: const CircleBorder(),
+            child: Icon(
+              item.icon,
+              color: itemColor,
+            ),
+            onTap: () {
+              if(item.onTap != null){
+                item.onTap!();
+                return;
+              }
+
+              onTap(index);
+              setState(() {});
+            },
+          )),
+    );
+  }
+
+  double _getCurrentCursorPosition(double itemWidth, double cursorWidth, int itemCount, int currentIndex) {
+    final double defaultPosition = itemWidth / 2 - cursorWidth/2;
+    double currentPosition = 0;
+    if (currentIndex <= itemCount) {
+      currentPosition = defaultPosition + (itemWidth * currentIndex);
+    }
+    return currentPosition;
+  }
+}
+
+
+
+class AFloatingNavigationItem {
+  final IconData icon;
+  final Function? onTap;
+
+
+  AFloatingNavigationItem({
+    required this.icon,
+    this.onTap
+  });
+}
+
